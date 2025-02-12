512084c299f1ff17d3ccd2f7b842770a146b4d5c
Edugonzalez <eduu.gonzalez@gmail.com>
2025-02-12 09:13:38 +0100

    Corrección

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -2,8 +2,8 @@ package com.campusdual.classroom;
 
 public class Exercise28 {
     public static void main(String[] args) {
-        int dividenado = 3;
+        int dividendo = 3;
         int divisor = 0;
-        System.out.println(3 / 0);
+        System.out.println(dividendo/divisor);
     }
 }
